87d852a00fc9c54c1e954d6a2ccbc0610d38cddd
Adrien DIDIER <adrien.didier@outlook.fr>
2022-06-26 19:30:12 +0200

    prep prod

--- main.py
+++ main.py
@@ -4,17 +4,15 @@ from twitter import *
 import schedule
 import time
 
-def job():    
+def job():
+    print("Start Execution")
     # DATABASE
     db = get_database()
 
     # GET ROAD closure
     df = get_data_table("https://www.cameroncountytx.gov/spacex/")
-    print(df)
     row_added, row_updated = insert_new_road_closure(db, df)
-    print(row_added, row_updated)
     dates_list = get_rc_to_check(db)
-    print(dates_list)
 
     # GET INFOS ABOUT FLIGHT DURING ROAD closure
     df_flight = get_infos_flight("https://www.cameroncountytx.gov/spacex/", dates_list)
@@ -29,10 +27,13 @@ def job():
     df_to_tweet = pd.concat([df_created, df_updated])
 
     if len(df_to_tweet) > 0:
+        print(f"Update / Creation of {len(df_created) + len(df_updated)} RC.")
         api = connect_api_twitter()
         tweet_road_closure(api, df_to_tweet)
 
-schedule.every(1).minutes.do(job)
+    print("Stop Execution")
+
+schedule.every(5).minutes.do(job)
 
 while 1:
     schedule.run_pending()
