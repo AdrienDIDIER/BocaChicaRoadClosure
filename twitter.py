2d0543b2dd784b58ed635260cf1e1f1703b23c59
Adrien DIDIER <adrien.didier@outlook.fr>
2022-08-15 17:42:26 +0200

    azdazd

--- twitter.py
+++ twitter.py
@@ -35,11 +35,17 @@ def tweet_road_closure(api, df):
     for _, row in df.iterrows():
         
         if row['created'] == True:
-            img = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_new.png")
+            img_tmp = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_new.png")
+            img = img_tmp.copy()
+            img_tmp.close()
         elif "Canceled" in row["Status"]:
-            img = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_canceled.png")
+            img_tmp = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_canceled.png")
+            img = img_tmp.copy()
+            img_tmp.close()
         else:
-            img = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_update.png")
+            img_tmp = Image.open("/home/ubuntu/BocaChicaRoadClosure/images/Road_update.png")
+            img = img_tmp.copy()
+            img_tmp.close()
         
         Date_start = row["DateTime_Start"].strftime("%A, %B %d, %Y - %I:%M %p")
         Date_end = row["DateTime_Stop"].strftime("%A, %B %d, %Y - %I:%M %p")
