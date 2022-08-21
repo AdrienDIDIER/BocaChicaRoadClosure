508bc31097bc4717429fa9bff729771d04c0b684
Adrien DIDIER <adrien.didier@outlook.fr>
2022-08-21 17:12:14 +0200

    azdaz

--- scrap.py
+++ scrap.py
@@ -162,12 +162,17 @@ def getMSIB():
 def getTFR(url):
     url = url
 
-    header = {
-    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
-    "X-Requested-With": "XMLHttpRequest"
-    }
+    headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image"
+        "apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Accept-Encoding" :"gzip, deflate",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Dnt": "1",
+        "Host": "httpbin.org",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",}
 
-    r = requests.get(url, headers=header)
+    r = requests.get(url, headers=headers)
     print(r.text)
     df = pd.read_html(
         r.text,
