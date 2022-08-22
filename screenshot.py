044f2bc61e1d05f88c7ab1ea090260370fb08bb8
Adrien DIDIER <adrien.didier@outlook.fr>
2022-08-22 09:45:38 +0200

    debug

--- screenshot.py
+++ screenshot.py
@@ -7,11 +7,12 @@ from selenium.webdriver.chrome.options import Options
 
 load_dotenv()
 
+CHROME_PATH = os.getenv('CHROME_PATH')
 CHROMEDRIVER_PATH = os.getenv('CHROMEDRIVER_PATH')
 WINDOW_SIZE = "1920,1080"
 
 chrome_options = Options()  
-chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
 def make_screenshot(url):
@@ -19,6 +20,7 @@ def make_screenshot(url):
         raise Exception('URLs need to start with "http"')
 
     driver = webdriver.Chrome(
+        binary_location=CHROME_PATH,
         executable_path=CHROMEDRIVER_PATH,
         chrome_options=chrome_options
     )  
