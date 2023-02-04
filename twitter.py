e9867f12247423bf0402e8eeaa72dd956d4497e9
Adrien DIDIER <adrien.didier@outlook.fr>
2023-02-04 16:20:43 +0100

    test

--- twitter.py
+++ twitter.py
@@ -25,6 +25,12 @@ from PIL import Image, ImageFont, ImageDraw
 def connect_page_twitter():
     options = Options()
     options.add_argument('--headless')
+    options.add_argument("start-maximized")
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options)
     driver.get('https://twitter.com/')
