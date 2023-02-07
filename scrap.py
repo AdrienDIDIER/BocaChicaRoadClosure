a6272f3055a45a267d558e42aec0819c2eedc201
Adrien DIDIER <adrien.didier@outlook.fr>
2023-02-07 11:16:28 +0100

    debug

--- scrap.py
+++ scrap.py
@@ -171,6 +171,7 @@ def getMSIB():
     options.add_argument('--headless')
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
 
